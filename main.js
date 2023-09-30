e48e4ea186b03770ab1e3811a3e5b7a6f2fdfec0
Anamonyus <126729826+Anamonyus@users.noreply.github.com>
2023-09-30 09:17:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 /*created by prashant shukla */
 
+
+
 var paddle2 =10,paddle1=10;
 
 var paddle1X = 10,paddle1Height = 110;
@@ -23,6 +25,18 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded)
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+  console.log("poseNet is Initialized");
 }
 
 
